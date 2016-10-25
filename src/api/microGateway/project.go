f7fbb67fe8cc3dee2c5c9d3ccc92412b20aae42f
Vital Leshchyk <VitalLeshchyk@gmail.com>
2016-10-25 10:34:00 +0300

    mono repository structure update

---
+++ src/api/microGateway/project.go
@@ -0,0 +1,167 @@
+package microGateway
+
+import (
+	"api/models"
+	proto "api/proto/project"
+	"api/restapi/operations/project"
+	"fmt"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/micro/go-micro"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+)
+
+//IProjectGateway is go-micro gateway for project
+type IProjectGateway interface {
+	CreateProjectAction(params project.CreateProjectParams) middleware.Responder
+	ReadProjectAction(params project.ReadProjectParams) middleware.Responder
+	UpdateProjectAction(params project.UpdateProjectParams) middleware.Responder
+	ListProjectsAction(params project.ListProjectsParams) middleware.Responder
+}
+
+type projectGateway struct {
+	service micro.Service
+}
+
+//NewProjectGateway returns go-micro gateway for project
+func NewProjectGateway() IProjectGateway {
+	return &projectGateway{
+		service: micro.NewService(micro.Name("project.client")),
+	}
+}
+
+//CreateProjectAction sends create request to micro-service
+func (g *projectGateway) CreateProjectAction(params project.CreateProjectParams) middleware.Responder {
+	service := micro.NewService(micro.Name("project.client"))
+
+	proj := proto.NewProjectClient("project", service.Client())
+
+	rsp, err := proj.Create(context.TODO(), &proto.CreateRequest{
+		Name:        params.Body.Name,
+		Description: params.Body.Description,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return project.NewCreateProjectInternalServerError()
+	}
+
+	if uint32(codes.OK) == rsp.Status {
+		fmt.Println(fmt.Sprintf("project.client: ok. Id = %v", rsp.Id))
+	} else {
+		fmt.Println("project.client: create fail. ")
+	}
+
+
+	readRsp, err := proj.Read(context.TODO(), &proto.ReadRequest{
+		Id: rsp.Id,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return project.NewCreateProjectInternalServerError()
+	}
+
+	pr := &models.Project{
+		ID:          readRsp.Project.Id,
+		Name:        readRsp.Project.Name,
+		Description: readRsp.Project.Description,
+	}
+
+	return project.NewCreateProjectCreated().WithPayload(pr)
+}
+
+//ReadProjectAction read project from micro-service
+func (g *projectGateway) ReadProjectAction(params project.ReadProjectParams) middleware.Responder {
+	service := micro.NewService(micro.Name("project.client"))
+
+	proj := proto.NewProjectClient("project", service.Client())
+	readRsp, err := proj.Read(context.TODO(), &proto.ReadRequest{
+		Id: params.ID,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return project.NewCreateProjectInternalServerError()
+	}
+
+	if uint32(codes.OK) == readRsp.Status {
+		fmt.Println(fmt.Sprintf("project.client read: ok. Id = %v", params.ID))
+	} else if uint32(codes.NotFound) == readRsp.Status {
+		return project.NewReadProjectNotFound()
+	}
+
+	pr := &models.Project{
+		ID:          readRsp.Project.Id,
+		Name:        readRsp.Project.Name,
+		Description: readRsp.Project.Description,
+	}
+
+	return project.NewReadProjectOK().WithPayload(pr)
+}
+
+//ReadProjectAction sends update request to micro-service
+func (g *projectGateway) UpdateProjectAction(params project.UpdateProjectParams) middleware.Responder {
+	service := micro.NewService(micro.Name("project.client"))
+
+	proj := proto.NewProjectClient("project", service.Client())
+
+	// request the Hello method on the Greeter handler
+	rsp, err := proj.Update(context.TODO(), &proto.UpdateRequest{
+		Id:          params.ID,
+		Name:        params.Body.Name,
+		Description: params.Body.Description,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return project.NewCreateProjectInternalServerError()
+	}
+
+	if uint32(codes.OK) == rsp.Status {
+		fmt.Println(fmt.Sprintf("project.client update: ok. Id = %v", params.ID))
+	} else if uint32(codes.NotFound) == rsp.Status {
+		return project.NewUpdateProjectNotFound()
+	} else {
+		fmt.Println(fmt.Sprintf("project.client: update fail. Id = %v, status = %v", params.ID, rsp.Status))
+		return project.NewCreateProjectInternalServerError()
+	}
+
+	readRsp, err := proj.Read(context.TODO(), &proto.ReadRequest{
+		Id: params.ID,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return project.NewCreateProjectInternalServerError()
+	}
+
+	pr := &models.Project{
+		ID:          readRsp.Project.Id,
+		Name:        readRsp.Project.Name,
+		Description: readRsp.Project.Description,
+	}
+
+	return project.NewUpdateProjectOK().WithPayload(pr)
+}
+
+//ListProjectsAction get all of projects from micro-service
+func (g *projectGateway) ListProjectsAction(params project.ListProjectsParams) middleware.Responder {
+	service := micro.NewService(micro.Name("project.client"))
+
+	proj := proto.NewProjectClient("project", service.Client())
+	listRsp, err := proj.List(context.TODO(), &proto.ListRequest{})
+
+	if err != nil {
+		fmt.Println(err)
+		return project.NewCreateProjectInternalServerError()
+	}
+
+	var projects = []*models.Project{}
+	for _, projResp := range listRsp.Projects {
+		p := &models.Project{
+			ID:          projResp.Id,
+			Name:        projResp.Name,
+			Description: projResp.Description,
+		}
+		projects = append(projects, p)
+	}
+
+	return project.NewListProjectsOK().WithPayload(projects)
+}
---
+++ src/project/handler/project_handler_test.go
@@ -0,0 +1,206 @@
+package handler
+
+import (
+	"github.com/stretchr/testify/suite"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"project/model"
+	proto "project/proto/project"
+	"testing"
+)
+
+type projectHandlerTestSuite struct {
+	suite.Suite
+}
+
+func (suite *projectHandlerTestSuite) TestCreate() {
+	ctx := context.TODO()
+	req := &proto.CreateRequest{
+		Name:        "Test Project",
+		Description: "Test Description",
+	}
+	rsp := new(proto.CreateResponse)
+
+	gateway := new(projectGatewayMock)
+	handler := &ProjectHandler{
+		Gateway: gateway,
+	}
+
+	handler.Create(ctx, req, rsp)
+
+	suite.Equal(rsp.Status, uint32(codes.OK))
+	suite.Equal(rsp.Id, uint64(1))
+	suite.Equal(gateway.StoredProject.Name, req.Name)
+	suite.Equal(gateway.StoredProject.Description, req.Description)
+}
+
+func (suite *projectHandlerTestSuite) TestRead() {
+	gateway := new(projectGatewayMock)
+	gateway.Insert(&model.Project{
+		Name:        "N",
+		Description: "D",
+	})
+\
+	ctx := context.TODO()
+	req := &proto.ReadRequest{
+		Id: 1,
+	}
+	rsp := new(proto.ReadResponse)
+
+	handler := &ProjectHandler{
+		Gateway: gateway,
+	}
+
+	handler.Read(ctx, req, rsp)
+
+	suite.Equal(rsp.Status, uint32(codes.OK))
+	suite.Equal(rsp.Project.Id, uint64(1))
+	suite.Equal(gateway.StoredProject.Name, rsp.Project.Name)
+	suite.Equal(gateway.StoredProject.Description, rsp.Project.Description)
+}
+
+func (suite *projectHandlerTestSuite) TestReadNotFound() {
+	gateway := new(projectGatewayMock)
+	gateway.Insert(&model.Project{
+		Name:        "N",
+		Description: "D",
+	})
+
+	ctx := context.TODO()
+	req := &proto.ReadRequest{
+		Id: 17636356,
+	}
+	rsp := new(proto.ReadResponse)
+
+	handler := &ProjectHandler{
+		Gateway: gateway,
+	}
+
+	handler.Read(ctx, req, rsp)
+
+	suite.Equal(uint32(codes.NotFound), rsp.Status)
+	suite.Nil(rsp.Project)
+}
+
+func (suite *projectHandlerTestSuite) TestUpdate() {
+	gateway := new(projectGatewayMock)
+	gateway.Insert(&model.Project{
+		Name:        "N",
+		Description: "D",
+	})
+
+	ctx := context.TODO()
+	req := &proto.UpdateRequest{
+		Id:          1,
+		Name:        "N2",
+		Description: "D2",
+	}
+	rsp := new(proto.UpdateResponse)
+
+	handler := &ProjectHandler{
+		Gateway: gateway,
+	}
+
+	handler.Update(ctx, req, rsp)
+
+	suite.Equal(rsp.Status, uint32(codes.OK))
+	suite.Equal(gateway.StoredProject.Id, uint64(1))
+	suite.Equal(gateway.StoredProject.Name, req.Name)
+	suite.Equal(gateway.StoredProject.Description, req.Description)
+}
+
+func (suite *projectHandlerTestSuite) TestUpdateNotFound() {
+	gateway := new(projectGatewayMock)
+
+	ctx := context.TODO()
+	req := &proto.UpdateRequest{
+		Id:          uint64(13435322),
+		Name:        "N4",
+		Description: "D4",
+	}
+	rsp := new(proto.UpdateResponse)
+
+	handler := &ProjectHandler{
+		Gateway: gateway,
+	}
+
+	handler.Update(ctx, req, rsp)
+
+	suite.Equal(uint32(codes.NotFound), rsp.Status)
+	suite.Nil(gateway.StoredProject)
+}
+
+func (suite *projectHandlerTestSuite) TestList() {
+	gateway := new(projectGatewayMock)
+	gateway.Insert(&model.Project{
+		Name:        "N",
+		Description: "D",
+	})
+
+	ctx := context.TODO()
+	req := &proto.ListRequest{}
+	rsp := new(proto.ListResponse)
+
+	handler := &ProjectHandler{
+		Gateway: gateway,
+	}
+
+	handler.List(ctx, req, rsp)
+
+	suite.Len(rsp.Projects, 1)
+	suite.Equal(rsp.Projects[0].Id, gateway.StoredProject.Id)
+	suite.Equal(rsp.Projects[0].Name, gateway.StoredProject.Name)
+	suite.Equal(rsp.Projects[0].Description, gateway.StoredProject.Description)
+}
+
+type projectGatewayMock struct {
+	StoredProject *model.Project
+}
+
+func (mock *projectGatewayMock) Insert(p *model.Project) {
+	mock.StoredProject = p
+	mock.StoredProject.Id = 1
+}
+
+func (mock *projectGatewayMock) isNotFound(id uint64) bool {
+	var notFound bool
+	if nil == mock.StoredProject || mock.StoredProject.Id != id {
+		notFound = true
+	} else {
+		println(mock.StoredProject.Id, id)
+		notFound = false
+	}
+
+	return notFound
+}
+
+func (mock *projectGatewayMock) SelectById(id uint64) (*model.Project, bool) {
+	if mock.isNotFound(id) {
+		return &model.Project{}, true
+	}
+
+	return mock.StoredProject, false
+}
+
+func (mock *projectGatewayMock) Update(p *model.Project) bool {
+	var result bool
+	if mock.isNotFound(p.Id) {
+		result = false
+	} else {
+		mock.StoredProject = p
+		result = true
+	}
+
+	return result
+}
+
+func (mock *projectGatewayMock) SelectAll() []*model.Project {
+	var projects = []*model.Project{}
+	projects = append(projects, mock.StoredProject)
+
+	return projects
+}
+
+func TestProjectHandlerSuite(t *testing.T) {
+	suite.Run(t, new(projectHandlerTestSuite))
+}
