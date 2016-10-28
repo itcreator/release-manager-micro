70d7ebf7f887e27ef0de72059055b6c92bcc6bb8
Vital Leshchyk <VitalLeshchyk@gmail.com>
2016-10-28 03:21:20 +0300

    Semver service initialization update consul cluster

--- devops/docker/consul/scripts/boot.sh
+++ devops/docker/consul/scripts/boot.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 set -ex
 
-consul agent -server -bootstrap-expect 1 -data-dir /tmp/consul -node=agent-one
+consul agent -server -bootstrap-expect 1 -data-dir /tmp/consul -node=agent-one &
 
 i="0"
-while [ $i -lt 10 ] #waiting for cluster 10 seconds
+while [ $i -lt 20 ] #waiting for cluster 10 seconds
 do
-    xterm &
     i=$[$i+1]
     sleep 1
 
-    consul join service.api; consul join service.project
+    consul join service.api; consul join service.project; consul join service.semver
     RESULT=$?
     if [ $RESULT -eq 0 ]; then
         echo "Consul cluster is ready"
         consul members
-        exit 1
+
+        tail -f /dev/null #run empty process
     else
-        echo "cluster doesn't ready. waiting..\r\n"
+        echo "cluster doesn't ready. waiting...\r\n"
     fi
 
     consul members
