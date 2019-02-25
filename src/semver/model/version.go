490c4602356d3cd79c640501b6c54eca1ba8f88c
Vital Leshchyk <VitalLeshchyk@gmail.com>
2019-02-26 00:52:14 +0300

    Multi release support

--- src/semver/model/version.go
+++ src/semver/model/version.go
@@ -7,9 +7,9 @@ import (
 // Version entity for semantic versioning strategy
 type Version struct {
 	UUID        uuid.NullUUID `db:"uuid"`
-	ProjectUUID uuid.UUID `db:"project_uuid"`
-	Major       uint32 `db:"major"`
-	Minor       uint32 `db:"minor"`
-	Revision    uint32 `db:"revision"`
-	Branch      string `db:"branch"`
+	ProjectUUID uuid.UUID     `db:"project_uuid"`
+	Major       uint32        `db:"major"`
+	Minor       uint32        `db:"minor"`
+	Revision    uint32        `db:"revision"`
+	Branch      string        `db:"branch"`
 }
